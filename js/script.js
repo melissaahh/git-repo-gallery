cc09a2f186e5d7d8feacaf11963afa8202fcf8d6
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2025-01-06 23:10:03 -0500

    Updated formatting

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ const repoData = document.querySelector(".repo-data");
 const viewReposButton = document.querySelector(".view-repos");
 const filterInput = document.querySelector(".filter-repos");
 
-const gitUserInfo = async function (){
-    const userInfo = await fetch (`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
-    displayUserInfo(data);
+const gitUserInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+  displayUserInfo(data);
 };
 
 gitUserInfo();
 
-const displayUserInfo = function (data){
-    const div = document.createElement("div");
-    div.classList.add("user-info");
-    div.innerHTML = `
+const displayUserInfo = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
         <figure>
             <img alt="user avatar" src=${data.avatar_url} />
         </figure>
@@ -28,83 +28,88 @@ const displayUserInfo = function (data){
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div> 
     `;
-    overview.append(div);
-    gitRepos();
+  overview.append(div);
+  gitRepos();
 };
 
-const gitRepos = async function (){
-    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
-    const repoData = await fetchRepos.json();
-    displayRepos(repoData);
+const gitRepos = async function () {
+  const fetchRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
 };
 
-const displayRepos = function (repos){
-    filterInput.classList.remove("hide");
-    for (const repo of repos) {
-        const repoItem = document.createElement("li");
-        repoItem.classList.add("repo");
-        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
-        repoList.append(repoItem);
-    }
+const displayRepos = function (repos) {
+  filterInput.classList.remove("hide");
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
 
-repoList.addEventListener("click", function (e){
-    if (e.target.matches("h3")) {
-        const repoName = e.target.innerText;
-        getRepoInfo(repoName);
-    }
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    getRepoInfo(repoName);
+  }
 });
 
-const getRepoInfo = async function (repoName){
-    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
-    const repoInfo = await fetchInfo.json();
-    console.log(repoInfo);
+const getRepoInfo = async function (repoName) {
+  const fetchInfo = await fetch(
+    `https://api.github.com/repos/${username}/${repoName}`
+  );
+  const repoInfo = await fetchInfo.json();
+  console.log(repoInfo);
 
-    const fetchLanguages = await fetch(repoInfo.languages_url);
-    const languageData = await fetchLanguages.json();
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
 
-    const languages = [];
-    for (const language in languageData){
-        languages.push(language);
-    }
+  const languages = [];
+  for (const language in languageData) {
+    languages.push(language);
+  }
 
-    displayRepoInfo(repoInfo, languages);
+  displayRepoInfo(repoInfo, languages);
 };
 
-const displayRepoInfo = function (repoInfo, languages){
-    viewReposButton.classList.remove("hide");
-    repoData.innerHTML = "";
-    repoData.classList.remove("hide");
-    allReposContainer.classList.add("hide");
-    const div = document.createElement("div");
-    div.innerHTML = `
+const displayRepoInfo = function (repoInfo, languages) {
+  viewReposButton.classList.remove("hide");
+  repoData.innerHTML = "";
+  repoData.classList.remove("hide");
+  allReposContainer.classList.add("hide");
+  const div = document.createElement("div");
+  div.innerHTML = `
         <h3>Name: ${repoInfo.name}</h3>
         <p>Description: ${repoInfo.description}</p>
         <p>Default Branch: ${repoInfo.default_branch}</p>
         <p>Languages: ${languages.join(", ")}</p>
-        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+        <a class="visit" href="${
+          repoInfo.html_url
+        }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
     `;
-    repoData.append(div);
+  repoData.append(div);
 };
 
-viewReposButton.addEventListener("click", function (){
-    allReposContainer.classList.remove("hide");
-    repoData.classList.add("hide");
-    viewReposButton.classList.add("hide");
+viewReposButton.addEventListener("click", function () {
+  allReposContainer.classList.remove("hide");
+  repoData.classList.add("hide");
+  viewReposButton.classList.add("hide");
 });
 
 filterInput.addEventListener("input", function (e) {
-    const searchText = e.target.value;
-    const repos = document.querySelectorAll(".repo");
-    const searchLowerText = searchText.toLowerCase();
+  const searchText = e.target.value;
+  const repos = document.querySelectorAll(".repo");
+  const searchLowerText = searchText.toLowerCase();
 
-    for (const repo of repos) {
-        const repoLowerText = repo.innerText.toLowerCase();
-        if (repoLowerText.includes(searchLowerText)) {
-            repo.classList.remove("hide");
-        } else {
-            repo.classList.add("hide");
-        }
+  for (const repo of repos) {
+    const repoLowerText = repo.innerText.toLowerCase();
+    if (repoLowerText.includes(searchLowerText)) {
+      repo.classList.remove("hide");
+    } else {
+      repo.classList.add("hide");
     }
+  }
 });
-
